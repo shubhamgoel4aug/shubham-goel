bb81d94231ed3739e1128d328eb956f4a06917d3
Shubham Goel <shgoel@deloitte.com>
2019-11-04 20:13:23 +0530

    Updated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyATznHMscADEt5P8tooo45CfGGIdzRLmhY",
+    authDomain: "shubham-goel.firebaseapp.com",
+    databaseURL: "https://shubham-goel.firebaseio.com",
+    projectId: "shubham-goel",
+    storageBucket: "shubham-goel.appspot.com",
+    messagingSenderId: "896866803983",
+    appId: "1:896866803983:web:113c4b30861142a0e15e7a",
+    measurementId: "G-32MYDZSXBN"
+  }
 };
